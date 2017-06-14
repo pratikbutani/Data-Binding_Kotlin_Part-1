8cbc4977210b31b3f3092b7c86275996a7e8711d
kevalamandroid <kevalamandroid@gmail.com>
2017-06-14 17:37:59 +0530

    Initial commit

---
+++ app/src/main/java/com/pratikbutani/kotlin/Data.kt
@@ -0,0 +1,6 @@
+package com.pratikbutani.kotlin
+
+/**
+ * Created by Pratik Butani on 14/06/17.
+ */
+data class Data(val name: String, val langName: String)
---
+++ app/src/main/java/com/pratikbutani/kotlin/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.pratikbutani.kotlin
+
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.pratikbutani.kotlin.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        /**
+         * Binding layout file
+         */
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        setSupportActionBar(binding.toolbar)
+
+        /**
+         * Creating Object of Data class
+         */
+        val data = Data("Pratik", "Kotlin")
+
+        // Setting Variables and their values.
+        binding.setVariable(BR.item, data)
+        binding.executePendingBindings()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id = item.itemId
+
+        if (id == R.id.action_settings) {
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+}
